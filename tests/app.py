39c498f97c2b49b004877036c0f06d38f3c77b2b
Daniil Bakushkin <dev.daniil.bakushkin@gmail.com>
2023-11-24 21:22:40 +0300

    add .gitignore

--- tests/app.py
+++
@@ -1,9 +0,0 @@
-class Car:
-    def __init__(self) -> None:
-        self.color = None
-
-    def set_color(self, color):
-        self.color = color
-
-    def get_color(self):
-        return self.color
